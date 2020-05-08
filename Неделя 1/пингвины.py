273a606fd82660392986f28a1c72bede407a04a3
homosociologicus <56775261+homosociologicus@users.noreply.github.com>
2020-05-09 00:56:17 +0300

    Add files via upload

---
+++ Неделя 1/пингвины.py
@@ -0,0 +1,11 @@
+q = int(input())
+line1 = '   _~_    '
+line2 = '  (o o)   '
+line3 = ' /  V  \  '
+line4 = '/(  _  )\ '
+line5 = '  ^^ ^^   '
+print(line1*q)
+print(line2*q)
+print(line3*q)
+print(line4*q)
+print(line5*q)
